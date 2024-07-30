c5969f46e5b2b6dffed6c56502581a1d70e047dd
brduarte <brunomacielduarte32@gmail.com>
2024-07-30 09:33:10 -0300

    feat: Flexibility header

--- src/views/Transaction/Layout.tsx
+++ src/views/Transaction/Layout.tsx
@@ -3,12 +3,19 @@ import {SafeAreaView} from '../../components/SafeAreaView/SafeAreaView.tsx';
 import {ModalHeader} from '../../navigate/modal/ModalHeader.tsx';
 import {styles} from './styles';
 import {View} from 'react-native';
+import {TransactionsList} from '../../components/TransactionsList/transactionsList.tsx';
+import {TransactionsModel} from '../../services/core/models/TransactionsModel.ts';
 
-export function Layout(): React.JSX.Element {
+type LayoutProps = {
+  transactions: TransactionsModel[];
+};
+
+export function Layout({transactions}: LayoutProps): React.JSX.Element {
   return (
     <SafeAreaView>
       <View style={styles.container}>
         <ModalHeader title={'Lançamentos'} />
+        <TransactionsList transactions={transactions} />
       </View>
     </SafeAreaView>
   );
--- src/views/Transaction/index.tsx
+++ src/views/Transaction/index.tsx
@@ -1,5 +1,31 @@
 import {Layout} from './Layout.tsx';
+import React, {useCallback, useState} from 'react';
+import {TransactionsModel} from '../../services/core/models/TransactionsModel.ts';
+import {useFocusEffect} from '@react-navigation/native';
+import {ITransactionService} from '../../services/core/interfaces/TransactionServiceInterface.ts';
+import {TransactionService} from '../../services/core/services/TransactionService.ts';
+import {useActiveIndicator} from '../../contexts/ActiveIndicatorContext.tsx';
 
 export function Transaction(): React.JSX.Element {
-  return <Layout />;
+  const activeIndicator = useActiveIndicator();
+
+  const [transactions, setTransactions] = useState<TransactionsModel[]>([]);
+
+  useFocusEffect(
+    useCallback(() => {
+      activeIndicator.active();
+
+      Promise.all([loadTransactions()]).finally(() => {
+        activeIndicator.disabled();
+      });
+    }, []),
+  );
+
+  async function loadTransactions(): Promise<void> {
+    const transactionService: ITransactionService = new TransactionService();
+
+    setTransactions(await transactionService.getAll());
+  }
+
+  return <Layout transactions={transactions} />;
 }
